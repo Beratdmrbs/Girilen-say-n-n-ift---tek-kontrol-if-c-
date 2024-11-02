e6c4af6476d4660f53cef46abe64b07213ac211f
Beratdmrbs <beratdmrbs61@gmail.com>
2024-11-02 20:20:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿double sayi = 0;
+Console.Write("Sayı giriniz:");
+sayi = Convert.ToDouble(Console.ReadLine());
+double kalan = sayi % 2;
+
+if (kalan == 0)
+{
+    Console.Write("Girdiğiniz sayı ÇİFT'tir.");
+}
+else
+{
+    Console.Write("Girdiğiniz sayı TEK'tir.");
+}
+Console.ReadKey();
